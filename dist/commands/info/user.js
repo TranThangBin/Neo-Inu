65adfbc0bf224359db62c8d08aea0e8b84051dd9
TranThangBin <thangdev04@gmail.com>
2023-05-08 12:40:56 +0700

    feat: /user now show the user avatar

--- dist/commands/info/user.js
+++ dist/commands/info/user.js
@@ -9,11 +9,20 @@ module.exports = {
         .setName("username")
         .setDescription("Provide information about a user in the server (blank if yourself).")),
     async execute(interaction) {
-        const target = interaction.options.getMember("name") ||
+        const target = interaction.options.getMember("username") ??
             interaction.member;
+        const targetAvatar = target.user.avatarURL();
+        if (!targetAvatar) {
+            await interaction.reply({
+                content: `This user is \`${target.user.username}\`, who joined on \`${target.joinedAt}\`.`,
+                ephemeral: true,
+            });
+            return;
+        }
         await interaction.reply({
             content: `This user is \`${target.user.username}\`, who joined on \`${target.joinedAt}\`.`,
             ephemeral: true,
+            files: [new discord_js_1.AttachmentBuilder(targetAvatar)],
         });
     },
 };
--- src/commands/info/user.ts
+++ src/commands/info/user.ts
@@ -2,6 +2,7 @@ import {
     SlashCommandBuilder,
     CommandInteraction,
     GuildMember,
+    AttachmentBuilder,
 } from "discord.js";
 
 module.exports = {
@@ -17,12 +18,23 @@ module.exports = {
         ),
     async execute(interaction: CommandInteraction) {
         const target =
-            (interaction.options.getMember("name") as GuildMember) ||
+            (interaction.options.getMember("username") as GuildMember) ??
             (interaction.member as GuildMember);
+
+        const targetAvatar = target.user.avatarURL() as string;
+
+        if (!targetAvatar) {
+            await interaction.reply({
+                content: `This user is \`${target.user.username}\`, who joined on \`${target.joinedAt}\`.`,
+                ephemeral: true,
+            });
+            return;
+        }
 
         await interaction.reply({
             content: `This user is \`${target.user.username}\`, who joined on \`${target.joinedAt}\`.`,
             ephemeral: true,
+            files: [new AttachmentBuilder(targetAvatar)],
         });
     },
 };
